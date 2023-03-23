bb022e9725b05dd08cfc0f79a1764f84e9a3e58c
Utkatsh Kaule <utkarshkaule7@gmail.com>
2023-03-23 22:40:10 +0530

    NavBar and Right Side Menu
    NavBar and Right Side Menu is done.

---
+++ youtube_clone/src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect } from 'react'
+import { Context } from '../context/contextApi'
+import LeftNav from './LeftNav'
+
+
+const Feed = () => {
+  return (
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <LeftNav />
+    </div>
+  )
+}
+
+export default Feed
---
+++ youtube_clone/src/context/contextApi.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { fetchDataFromApi } from '../utils/api';
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState(false);
+    const [selectedCategories, setSelectedCategories] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategories);
+    }, [selectedCategories]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({contents}) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        })
+    }
+
+    return <Context.Provider value={{
+        loading,
+        setLoading,
+        searchResults,
+        setSearchResults,
+        selectedCategories,
+        setSelectedCategories,
+        mobileMenu,
+        setMobileMenu
+    }}>
+        {props.children}
+    </Context.Provider>
+}
